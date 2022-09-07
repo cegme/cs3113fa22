761104c753ce3c72fdb71fec1eef2872178120bd
Christan Grant <cgrant@ou.edu>
2022-09-07 15:38:25 +0000

    Added changed from lecture one. Ignoring .s files

---
+++ mycfile.c
@@ -0,0 +1,65 @@
+
+
+/* Hello i am typing a c comment */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define PI 3.14159265358979
+#define HALFOF(x) x/2
+#define TRUE 1
+#define FALSE 0
+
+
+int main(int argc, char** argv, char** envp) {
+
+	/*
+	 printf("Hello, World!\n");
+
+	// What is argc?
+	printf("argc: %d\n", argc); 
+
+	// Argv?? lets see it
+	for(int i = 0; i < argc; ++i) {
+		printf("\targv[%d]: %s\n", i, argv[i]);
+	}
+
+	// envp???
+	for(int i = 0; i < argc; ++i) {
+		printf("\tenvp[%d]: %s\n", i, envp[i]);
+	}
+
+	printf("PI: %f", PI);
+	printf("half of 100000: %d", HALFOF(100000));
+	printf("half of 100000: %d", 100000/2);
+
+	*/
+	// data types??
+	/*
+	 * int
+	 * char
+	 * long
+	 * short
+	 * double
+	 * long long
+	 * float
+	 * byte
+	 * unsigned char
+	 * unsigned int
+	 */
+
+	short one = 65530+1;
+	printf("1 = %d\n", one);
+
+
+
+	//int count;
+       	//int myvalue;	
+	//count = scanf("%d", &myvalue);
+
+	return 0;
+}
+
+
+
